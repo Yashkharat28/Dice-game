ae7d95dfaf5b14a332c2002c90560adef2c9f6ca
Yashkharat28 <85253318+Yashkharat28@users.noreply.github.com>
2021-06-13 12:18:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+
+function myFunction(){
+    location.reload();
+    
+}
+var randomNumber1;
+    var randomNumber2;
+    randomNumber1=Math.floor(Math.random()*6)+1;
+    randomNumber2=Math.floor(Math.random()*6)+1;
+    document.querySelector(".dice1 img").src="images/dice"+randomNumber1+".png";
+    document.querySelector(".dice2 img").src="images/dice"+randomNumber2+".png";
+    
+    if(randomNumber1>randomNumber2)
+    {
+        document.querySelector("h1").innerHTML="Player 1 Wins!"
+    }
+    else if(randomNumber2>randomNumber1){
+        document.querySelector("h1").innerHTML="Player 2 Wins!"
+    }
+    else{
+        document.querySelector("h1").innerHTML="Draw!"
+    }
+
+
+
+function myFunction1(){
+    document.querySelector("h1").innerHTML="Dice Game!";
+    document.querySelector(".dice1 img").src="images/dice6.png";
+    document.querySelector(".dice2 img").src="images/dice6.png";
+}
